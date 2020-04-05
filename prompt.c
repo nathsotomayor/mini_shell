0be71ffe69f2c7a9e96882d8b738502886b883bf
Nathaly <nathysoto12.07@gmail.com>
2020-04-05 04:25:32 +0000

    prints '$ ', wait for the user to enter a command, prints it on the next line

---
+++ prompt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints "$ ", wait for to enter a command, prints it on the next line.
+ *
+ * Return: Always 0 success
+ */
+
+int main(int ac, char *av[])
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+
+	printf("#cisfun$ ");
+	read = getline(&line, &len, stdin);
+	if (read < 0)
+	{
+		perror("Unable to allocate buffer");
+		return (EXIT_FAILURE);
+	}
+	printf("%s", line);
+	free(line);
+	return (0);
+}
